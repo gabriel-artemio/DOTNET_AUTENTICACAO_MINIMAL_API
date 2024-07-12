6318cef240e7e0f7cc6462483a0cb46c2841d984
Gabriel Artemio <43358004+gabriel-artemio@users.noreply.github.com>
2024-07-12 15:00:05 -0300

    crud tabela horario

--- agendaSalaoApi/Controllers/HorariosController.cs
+++ agendaSalaoApi/Controllers/HorariosController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using agendaSalaoApi.Models;
+using agendaSalaoApi.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace agendaSalaoApi.Controllers
 {
@@ -6,6 +8,59 @@ namespace agendaSalaoApi.Controllers
     [ApiController]
     public class HorariosController : ControllerBase
     {
-        
+        private readonly HorarioService _service;
+
+        public HorariosController(HorarioService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Horario>> GetAll()
+        {
+            return _service.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Horario> GetById(int id)
+        {
+            var servico = _service.GetById(id);
+
+            if (servico == null)
+                return NotFound();
+
+            return servico;
+        }
+        [HttpPost]
+        public IActionResult Create(Horario horario)
+        {
+            _service.Insert(horario);
+            return CreatedAtAction(nameof(GetById), new { id = horario.cd_horario }, horario);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Horario horario)
+        {
+            if (id != horario.cd_horario)
+                return BadRequest();
+
+            var existingServico = _service.GetById(id);
+            if (existingServico == null)
+                return NotFound();
+
+            _service.Update(id, horario);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var horario = _service.GetById(id);
+            if (horario == null)
+                return NotFound();
+
+            _service.Delete(id);
+            return NoContent();
+        }
     }
 }
--- agendaSalaoApi/Models/Horario.cs
+++ agendaSalaoApi/Models/Horario.cs
@@ -2,9 +2,11 @@
 {
     public class Horario
     {
-        public int Id { get; set; }
-        public DateTime DataHora { get; set; }
-        public int ServicoId { get; set; }
-        public string? TelefoneCabelereiro { get; set; }
+        public int cd_horario { get; set; }
+        public DateTime dt_atendimento { get; set; }
+        public string? telefone_cliente { get; set; }
+        public string? nome_cliente { get; set; }
+        public int cd_servico { get; set; }
+        public int cd_salao { get; set; }
     }
 }
