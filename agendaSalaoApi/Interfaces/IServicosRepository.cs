db741715be8a36ca2b56c25034b21a673b4e3784
Gabriel Artemio <43358004+gabriel-artemio@users.noreply.github.com>
2024-10-16 15:23:11 -0300

    Delete agendaSalaoApi directory

--- agendaSalaoApi/Interfaces/IServicosRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using agendaSalaoApi.Models;
-
-namespace agendaSalaoApi.Interfaces
-{
-    public interface IServicosRepository
-    {
-        Task<IEnumerable<Servico>> GetServicos();
-    }
-}
--- agendaSalaoApi/Models/Servico.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace agendaSalaoApi.Models
-{
-    public class Servico
-    {
-        public int cd_servico { get; set; }
-        public string? nm_servico { get; set; }
-        public decimal valor { get; set; }
-    }
-}
