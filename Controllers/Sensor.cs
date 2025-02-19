cc4207fc74346c61163027dc4779556deb11e541
Gabriel Artemio <43358004+gabriel-artemio@users.noreply.github.com>
2025-02-19 14:54:52 -0300

    leitura sensor dht

---
+++ Controllers/Sensor.cs
@@ -0,0 +1,21 @@
+﻿using IntegraArduinoApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/sensor")]
+[ApiController]
+public class SensorController : ControllerBase
+{
+    private readonly SerialPortService _serialService;
+
+    public SensorController(SerialPortService serialService)
+    {
+        _serialService = serialService;
+    }
+
+    [HttpGet]
+    public IActionResult GetSensorData()
+    {
+        var (temperatura, umidade) = _serialService.ObterUltimaLeitura();
+        return Ok(new { temperatura, umidade });
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+using IntegraArduinoApi.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddSingleton<SerialPortService>(); // Adiciona o serviço da porta serial
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
